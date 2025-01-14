52c1743ce52dd92bc23370d8e4ada44e1e8beced
rsaxb010 <audrey.bollas@nationwidechildrens.org>
2025-01-14 11:29:36 -0500

    add gentrome and transcript fasta

--- get_resources.sh
+++ get_resources.sh
@@ -2,7 +2,7 @@
 
 resources="${1:-resources}"
 
-# Download reference data
+# Reference data
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_43/GRCh38.p13.genome.fa.gz -P $resources
 gunzip -c $resources/GRCh38.p13.genome.fa.gz > $resources/GRCh38.p13.genome.fa
 gatk CreateSequenceDictionary -R $resources/GRCh38.p13.genome.fa
@@ -11,9 +11,11 @@ samtools faidx $resources/GRCh38.p13.genome.fa
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_43/gencode.v43.primary_assembly.annotation.gtf.gz -P $resources
 gunzip -c $resources/gencode.v43.primary_assembly.annotation.gtf.gz > $resources/gencode.v43.primary_assembly.annotation.gtf
 
-# Salmon index
-refgenie init -c $resources/genome_config.yaml
-refgenie pull -c $resources/genome_config.yaml hg38/salmon_partial_sa_index:default
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_47/gencode.v47.transcripts.fa.gz -P $resources
+
+# Salmon
+wget https://github.com/COMBINE-lab/salmon/releases/download/v1.9.0/salmon-1.9.0_linux_x86_64.tar.gz -P dependencies/
+tar -xvzf dependencies/salmon-1.9.0_linux_x86_64.tar.gz -C dependencies/
 
 # Known sites for GATK BaseRecalibrator
 wget https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/00-common_all.vcf.gz -P $resources
